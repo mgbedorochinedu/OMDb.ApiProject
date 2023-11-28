6ce80f605ac5a7a59f38e6e4311afbd80451c5f8
Chinedu Mgbedoro <Chinedu.Mgbedoro@sterling.ng>
2023-11-28 16:53:27 +0100

    Add swagger authorize to pass the APIKey value

--- OMDb.API/Program.cs
+++ OMDb.API/Program.cs
@@ -22,8 +22,32 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    
+    c.AddSecurityDefinition("ApiKey", new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Description = "The API Key to access the API",
+        Type = SecuritySchemeType.ApiKey,
+        Name = "x-api-key",
+        In = ParameterLocation.Header,
+        Scheme = "ApiKeyScheme"
+    });
+
+    var scheme = new OpenApiSecurityScheme
+    {
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "ApiKey"
+        },
+        In = ParameterLocation.Header,
+    };
+
+    var requirement = new OpenApiSecurityRequirement
+    {
+        { scheme, new List<string>() }
+    };
+    c.AddSecurityRequirement(requirement);
 });
+
 
 builder.Services.AddScoped<ApiKeyAuthFilter>(); //Have control which endpoint to Authorized
 
